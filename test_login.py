6ea9f9eb1067b848a36810a800ad2ce79e5af706
Runnels Benjamin 43663405 <benjamin.runnels@ubs.com>
2021-11-15 17:15:07 +0000

    added additional tests for xsdata xml serialized output and soapUI xml will parse added test to demonstrate removing the namespace from Result will allow the MFNA xml response to parse

--- test_login.py
+++ test_login.py
@@ -4,9 +4,10 @@ from unittest import TestCase
 
 from xsdata.formats.dataclass.client import Client
 from xsdata.formats.dataclass.parsers import XmlParser
+from xsdata.formats.dataclass.serializers import XmlSerializer
 
 from mfna import NetworkManagementApiLoginOutput, NasFault, Result, NetworkManagementApiLogin, LoginRequest, \
-    LoginInputParms, RequestHeader, NetworkManagementApiLoginInput
+    LoginInputParms, RequestHeader, NetworkManagementApiLoginInput, LoginResponse
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -41,6 +42,34 @@ success_xml = '''\
     </nas:loginResponse>
   </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>
+'''
+
+xsdata_success_xml = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<soap-env:Envelope xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap-env:Body>
+    <ns1:loginResponse xmlns:ns1="http://microfocus.com/nas/2020/08">
+      <Result>
+        <ns1:Status>200 Logged in</ns1:Status>
+        <ns1:Text>s229dc048-746e-471e-aaf8-66f7317d89d7</ns1:Text>
+      </Result>
+    </ns1:loginResponse>
+  </soap-env:Body>
+</soap-env:Envelope>
+'''
+
+soap_ui_success_xml = '''\
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://microfocus.com/nas/2020/08">
+ <soapenv:Header/>
+ <soapenv:Body>
+    <ns:loginResponse>
+       <Result>
+          <ns:Status>200 Logged in</ns:Status>
+          <ns:Text>s229dc048-746e-471e-aaf8-66f7317d89d7</ns:Text>
+       </Result>
+    </ns:loginResponse>
+ </soapenv:Body>
+</soapenv:Envelope>
 '''
 
 login_envelope = '<?xml version="1.0" encoding="UTF-8"?><soap-env:Envelope ' \
@@ -84,6 +113,36 @@ class LoginOutputParserTests(TestCase):
     def test_success(self):
         response = self.parser.from_string(success_xml, self.output)
         self.assertIsInstance(response, self.output)
+        self.assertIsInstance(response.body.login_response.result, Result)
+        self.assertEqual("200 Logged in", response.body.login_response.result.status)
+        self.assertEqual("s229dc048-746e-471e-aaf8-66f7317d89d7", response.body.login_response.result.text)
+
+    def test_soap_result_without_namespace(self):
+        response = self.parser.from_string(success_xml.replace("nas:Result>", "Result>"), self.output)
+        self.assertIsInstance(response, self.output)
+        self.assertIsInstance(response.body.login_response.result, Result)
+        self.assertEqual("200 Logged in", response.body.login_response.result.status)
+        self.assertEqual("s229dc048-746e-471e-aaf8-66f7317d89d7", response.body.login_response.result.text)
+
+    def test_soapui_success(self):
+        response = self.parser.from_string(soap_ui_success_xml, self.output)
+        self.assertIsInstance(response, self.output)
+        self.assertIsInstance(response.body.login_response.result, Result)
+        self.assertEqual("200 Logged in", response.body.login_response.result.status)
+        self.assertEqual("s229dc048-746e-471e-aaf8-66f7317d89d7", response.body.login_response.result.text)
+
+    def test_soap_response(self):
+        test_out = NetworkManagementApiLoginOutput()
+        test_out.body = NetworkManagementApiLoginOutput.Body()
+        test_out.body.login_response = LoginResponse()
+        test_out.body.login_response.result = Result()
+        test_out.body.login_response.result.status = "200 Logged in"
+        test_out.body.login_response.result.text = "s229dc048-746e-471e-aaf8-66f7317d89d7"
+        serializer = XmlSerializer()
+        serializer.config.pretty_print = True
+        xsdata_xml = serializer.render(test_out)
+        self.assertEqual(xsdata_success_xml, xsdata_xml)
+        response = self.parser.from_string(xsdata_xml, self.output)
         self.assertIsInstance(response.body.login_response.result, Result)
         self.assertEqual("200 Logged in", response.body.login_response.result.status)
         self.assertEqual("s229dc048-746e-471e-aaf8-66f7317d89d7", response.body.login_response.result.text)
